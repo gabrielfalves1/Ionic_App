f69a2a57e2ff156c391a1e83e87a6f64441f861e
Gabriel Alves <bielalvesferreira2003@gmail.com>
2023-05-19 21:38:27 -0300

    valdiar form

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -2,13 +2,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 export class User {
 
-    _id: string = uuid();
+    _id: string = uuidv4();
     nome: string = "";
     email: string = "";
     senha: string = "";
     telefone: string = "";
-
-
 
 }
 
