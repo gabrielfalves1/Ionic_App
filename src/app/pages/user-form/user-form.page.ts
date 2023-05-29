ae51055b66d118a2be8a063db58b51e182d209d1
Fabiano Moreira <fabianomoreira.net@gmail.com>
2023-05-29 20:13:52 -0300

    aula 06

---
+++ src/app/pages/user-form/user-form.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.page.html',
+  styleUrls: ['./user-form.page.scss'],
+})
+export class UserFormPage implements OnInit {
+
+  ngOnInit() {
+  }
+
+  constructor(
+    private alertController: AlertController,
+    private userService: UserService
+  ) { }
+
+  user = new User()
+
+  async presentAlert(tipo: string, texto: string) {
+    const alert = await this.alertController.create({
+      header: tipo,
+      //subHeader: 'Important message',
+      message: texto,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  save() {
+    //this.userService.add(this.user);
+    console.log(this.user);
+    this.presentAlert("Aviso", "Cadastrado");
+  }
+
+
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { User } from '../model/user';
-import { AlertController } from '@ionic/angular';
-import { UserService } from '../services/user-service';
+
+
 
 @Component({
   selector: 'app-tab2',
@@ -9,30 +8,6 @@ import { UserService } from '../services/user-service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor(
-    private alertController: AlertController,
-    //private userService:UserService
-  ) { }
-
-  user = new User()
-  userService = new UserService();
-
-  async presentAlert(tipo: string, texto: string) {
-    const alert = await this.alertController.create({
-      header: tipo,
-      //subHeader: 'Important message',
-      message: texto,
-      buttons: ['OK'],
-    });
-    await alert.present();
-  }
-
-  save() {
-    this.userService.add(this.user);
-    console.log(this.user);
-    this.presentAlert("Aviso", "Cadastrado");
-  }
 
 
 }
